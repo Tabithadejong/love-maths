acbced49e94b2ef8267fe8af66ffdeb6d1649b66
Tabithadejong <tabithadejong654@gmail.com>
2022-09-24 17:28:35 +0000

    Set-up functions basis for game

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+function runGame() {
+
+ }
+function checkAnswer(){ 
+
+}
+function caculateCorrectAnswer(){ 
+
+}
+function incrementScore(){
+
+}
+function incrementWrongAnswer(){ 
+
+}
+function displayAdditionQuestion(){ 
+
+}
+function displaySubtractnQuestion(){ 
+
+}
+function displayMultiplyQuestion(){ 
+    
+}
