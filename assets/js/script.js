8454479970bd6305bfd1c37215b564d67c26c68e
Tabithadejong <tabithadejong654@gmail.com>
2022-09-25 07:28:55 +0000

    write code for subtract/default game to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,10 +18,18 @@ document.addEventListener("DOMContentLoaded", function() {
  * The main game " loop", called when the scirpt is first loaded 
  * and after the user's answer has been processed
  */
-function runGame() {
+function runGame(gameType) {
     let num1 = Math.floor(Math.random() *25) +1; 
     let num2 = Math.floor(Math.random() *25) +1; 
+
+    if (gameType === "addition") { 
+        displayAdditionQuestion(num1, num2); 
+    } else { 
+        alert(`Unknown game type: ${gameType}`); 
+        throw `Unknown game type: ${gameType}. Aborting`; 
+    }
  }
+
 
 
 function checkAnswer(){ 
@@ -36,8 +44,11 @@ function incrementScore(){
 function incrementWrongAnswer(){ 
 
 }
-function displayAdditionQuestion(){ 
-
+function displayAdditionQuestion(operand1, operand2){ 
+    document.getElementById('operand1').textContent = operand1; 
+    document.getElementById("operand2").textContent = operand2; 
+    document.getElementById("operator").textContent = "+"; 
+   
 }
 function displaySubtractnQuestion(){ 
 
